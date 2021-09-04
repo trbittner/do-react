5d3e44b816fa5987ca8bd2341c550520d734be21
Todd Bittner <toddbittner@gmail.com>
2021-09-04 09:29:08 -0500

    Changed the component from a class to a function.

--- digital-ocean-tutorial/src/components/Instructions/Instructions.js
+++ digital-ocean-tutorial/src/components/Instructions/Instructions.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react'
+import React from 'react'
 import emoji from './emoji.svg'
 import './Instructions.css'
 
-export default class Instructions extends Component {
-  render() {
-    return(
-      <div className="instructions">
-      <img alt="laughing crying emoji" src={emoji} />
-      <p>Click on an emoji to view the emoji short name.</p>
-      </div>
-    )
-  }
+const Instructions = () => {
+  return(
+    <div className="instructions">
+    <img alt="laughing crying emoji" src={emoji} />
+    <p>Click on an emoji to view the emoji short name.</p>
+    </div>
+  )
 }
+
+export default Instructions
