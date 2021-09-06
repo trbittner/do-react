6c8c897a7cbd32a3a2cde09d5e7097a68722299f
Todd Bittner <toddbittner@gmail.com>
2021-09-06 17:08:37 -0500

    Created a new root component for a style tutorial.

---
+++ digital-ocean-tutorial/src/components/Style/Style.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Style.css'
+
+import Alert from '../Alert/Alert'
+
+function Style() {
+  return (
+      <div className="wrapper">
+        <Alert />
+      </div>
+    )
+}
+
+export default Style
--- digital-ocean-tutorial/src/index.js
+++ digital-ocean-tutorial/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
+import Style from './components/Style/Style';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Style />
   </React.StrictMode>,
   document.getElementById('root')
 );
