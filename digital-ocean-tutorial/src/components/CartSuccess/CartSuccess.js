d2d1bdb44188efe46fff96f8825f3019408891be
Todd Bittner <toddbittner@gmail.com>
2021-09-12 12:59:38 -0500

    Styled using React components.

--- digital-ocean-tutorial/src/components/CartSuccess/CartSuccess.js
+++ digital-ocean-tutorial/src/components/CartSuccess/CartSuccess.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import Alert from '../Alert/Alert'
-import './CartSuccess.css'
 
 export default function CartSucess() {
+  const styles = {
+    header: {
+      width: '100%'
+      
+    },
+    item: {
+      marginRight: 20
+    },
+    wrapper: {
+      borderTop: 'black solid 1px',
+      display: 'flex',
+      flexWrap: 'wrap'
+    }
+  }
   return(
     <Alert title="Added to Cart" type="success">
-      <div className="cart-success-wrapper">
-        <h2>
+      <div style={styles.wrapper}>
+        <h2 style={styles.header}>
           You have added 3 items:
         </h2>
-        <div className="item">
+        <div style={styles.item}>
           <div>Bananas</div>
           <div>Quantity: 2</div>
         </div>
-        <div className="item">
+        <div styel={styles.item}>
           <div>Lettuce</div>
           <div>Quantity: 1</div>
         </div>
--- digital-ocean-tutorial/src/components/Style/Style.js
+++ digital-ocean-tutorial/src/components/Style/Style.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import './Style.css'
 
 import Alert from '../Alert/Alert'
 import CartSucess from '../CartSuccess/CartSuccess'
 
+
 function Style() {
+  const wrapper = {
+    padding: 20
+  }
+  
   return (
-      <div className="wrapper">
+      <div style={wrapper}>
         <Alert title="Items Not Added" type="error">
          <div>Your items are out of stock.</div>
         </Alert>
