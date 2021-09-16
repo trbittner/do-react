6bcdb618c1c74c8f3fc29fcd2c22ae4dd5911d01
Todd Bittner <toddbittner@gmail.com>
2021-09-15 19:26:45 -0500

    Added initial state.

--- digital-ocean-tutorial/src/components/Product/Product.js
+++ digital-ocean-tutorial/src/components/Product/Product.js
@@ -2,16 +2,37 @@ import React, { Component } from  'react'
 import './Product.css'
 
 export default class Product extends Component {
+  state = {
+    cart: [],
+    total: 0
+  }
+  
+  add = () => {
+    this.setState({
+      cart: ['ice cream'],
+      total: 5
+    })
+  }
+  
+  currencyOptions = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }
+  
+  getTotal = () => {
+    return this.state.total.toLocaleString(undefined,this.currencyOptions)
+  }
+  
   render() {
     return (
       <div className="wrapper">
         <div>
-          Shopping Cart: 0 total items.
+          Shopping Cart: {this.state.cart.length} total items.
         </div>
-        <div>Total: 0</div>
+        <div>Total: {this.getTotal()}</div>
         
         <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-        <button>Add</button> <button>Remove</button>
+        <button onClick={this.add}>Add</button> <button>Remove</button>
       </div>
     )
   }
