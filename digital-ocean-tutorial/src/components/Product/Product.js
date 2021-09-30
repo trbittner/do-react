19401eb0b2c287bffea7b5edd8dcc6c711004be5
Todd Bittner <toddbittner@gmail.com>
2021-09-30 15:41:44 -0500

    Updated Product to handle state via hooks.

--- digital-ocean-tutorial/src/components/Product/Product.js
+++ digital-ocean-tutorial/src/components/Product/Product.js
@@ -1,4 +1,4 @@
-import React, { Component } from  'react'
+import React from 'react'
 import './Product.css'
 
 const products = [
@@ -19,58 +19,15 @@ const products = [
   }
 ]
 
-export default class Product extends Component {
-  state = {
-    cart: [],
-  }
-
-  add = (product) => {
-    this.setState(state => ({
-      cart: [...state.cart,product]
-    }))
-  }
-
-  remove = (product) => {
-    this.setState(state => {
-      const cart = [...state.cart]
-      const productIndex = cart.findIndex(p => p.name === product.name)
-      if (productIndex < 0) {
-        return
-      }
-      cart.splice(productIndex,1)
-      return({
-        cart
-      })
-    })
-  }
-
-  currencyOptions = {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  }
-
-  getTotal = () => {
-    const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price,0)
-    return total.toLocaleString(undefined,this.currencyOptions)
-  }
-
-  render() {
+export default function Product() {
     return (
       <div className="wrapper">
         <div>
-          Shopping Cart: {this.state.cart.length} total items.
+          Shopping Cart: 0 total items.
         </div>
-        <div>Total: {this.getTotal()}</div>
-        <div>
-        {products.map(product => (
-          <div key={product.name}>
-            <div className="product"><span role="img" aria-label={product.name}>{product.emoji}</span></div>
-            <button onClick={() => this.add(product)}>Add</button>
-            <button onClick={() => this.remove(product)}>Remove</button>
-          </div>
-        ))}
-        </div>
+        <div>Total: 0</div>
+        <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+        <button>Add</button><button>Remove</button>
       </div>
     )
-  }
 }
