33ec600239bf845eebf4875978d3300e471b7a21
Todd Bittner <toddbittner@gmail.com>
2021-09-30 15:54:29 -0500

    Added two state components.

--- digital-ocean-tutorial/src/components/Product/Product.js
+++ digital-ocean-tutorial/src/components/Product/Product.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Product.css'
 
 const products = [
@@ -19,13 +19,25 @@ const products = [
   }
 ]
 
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+}
+
+function getTotal(total) {
+  return total.toLocaleString(undefined,currencyOptions)
+}
+
 export default function Product() {
+    const [cart,setCart] = useState([])
+    const [total,setTotal] = useState(0)
+
     return (
       <div className="wrapper">
         <div>
-          Shopping Cart: 0 total items.
+          Shopping Cart: {cart.length} total items.
         </div>
-        <div>Total: 0</div>
+        <div>Total: {getTotal(total)}</div>
         <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
         <button>Add</button><button>Remove</button>
       </div>
