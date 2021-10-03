293b0eba1cf21b9393f7ba5912ebbd8c346195d7
Todd Bittner <toddbittner@gmail.com>
2021-10-03 17:51:12 -0500

    Added working 'add' state to multiple products.

--- digital-ocean-tutorial/src/components/Product/Product.js
+++ digital-ocean-tutorial/src/components/Product/Product.js
@@ -32,14 +32,26 @@ export default function Product() {
     const [cart,setCart] = useState([])
     const [total,setTotal] = useState(0)
 
+    function add(product) {
+      setCart(current => [...current,product.name])
+      setTotal(current => current + product.price)
+    }
+
     return (
       <div className="wrapper">
         <div>
           Shopping Cart: {cart.length} total items.
         </div>
         <div>Total: {getTotal(total)}</div>
-        <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-        <button>Add</button><button>Remove</button>
+        {products.map(product => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>{product.emoji}</span>
+            </div>
+            <button onClick={() => add(product)}>Add</button>
+            <button>Remove</button>
+          </div>
+        ))}
       </div>
     )
 }
