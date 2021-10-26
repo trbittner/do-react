e66a57b260a2d9ffd359d36839332aed3d798d97
Todd Bittner <toddbittner@gmail.com>
2021-10-25 19:05:03 -0500

    Added items for salad composition.

--- digital-ocean-tutorial/src/components/SaladMaker/SaladMaker.js
+++ digital-ocean-tutorial/src/components/SaladMaker/SaladMaker.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
+import SaladBuilder from '../SaladBuilder/SaladBuilder'
 
 const useStyles = createUseStyles({
   wrapper: {
@@ -10,10 +11,13 @@ const useStyles = createUseStyles({
 export default function SaladMaker() {
   const classes = useStyles()
   return (
+    <>
     <h1 className={classes.wrapper}>
       <span role="img" aria-label="salad">🥗</span>
       Build Your Custom Salad!
       <span role="img" aria-label="salad">🥗</span>
     </h1>
+    <SaladBuilder />
+    </>
   )
 }
