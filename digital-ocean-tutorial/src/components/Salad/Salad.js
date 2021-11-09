66d22e5dad8f8aa00cd06e5a1776ef8b8916a23f
Todd Bittner <toddbittner@gmail.com>
2021-11-08 21:04:36 -0600

    Created a state provider for a User component.

--- digital-ocean-tutorial/src/components/Salad/Salad.js
+++ digital-ocean-tutorial/src/components/Salad/Salad.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navigation from "../Navigation/Navigation"
 import SaladMaker from "../SaladMaker/SaladMaker"
+import UserContext from "../User/User"
+
+const user = {
+  name: 'Kwame',
+  favorites: [
+    'avocado',
+    'carrot'
+  ]
+}
 
 function Salad() {
   return (
-    <>
+    <UserContext.Provider value={user}>
     <Navigation/>
     <SaladMaker/>
-    </>
+    </UserContext.Provider>
     )
 }
 
